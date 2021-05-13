750246f43d6fbd842a096b644ef5aab04e879960
Alyna Palamarchuk <alynapchuk@gmail.com>
2021-05-13 15:26:52 -0500

    Draft of our game project.

---
+++ ourgame.py
@@ -0,0 +1,238 @@
+# class Character:
+
+#     def __init__(self, health, power, name):
+#         pass
+
+#     def attack(self, target):
+#             if isinstance(target, Zombie):
+#                 print("The attack doesn't seem to have any effect.")
+#             else:
+#                 target.health -= self.power
+#                 print("%s does %d damage to %s!" % (self.name, self.power, target.name))
+
+#     def sleep(self, target):
+#         self.health -= target.power
+#         print("%s does %d damange to %s!" % (target.name, target.power, self.name))
+
+#     def alive(self):
+#         if (self.health > 0):
+#             return True
+#         elif (self.health <= 0):
+#             print("RIP in peace, %s..." % self.name)
+#             print()
+#         elif (target.health <= 0):
+#             print("RIP in peace, %s..." % target.name)
+#             print()
+
+#     def print_status(self):
+#         print("%s has %d health and %d power." % (self.name, self.health, self.power))
+
+
+
+
+
+
+#CLASSES
+class Hero:
+    def __init__(self, name, health, power):
+        self.name = name
+        self.power = power
+        self.health = health
+
+    def hero_status(self):
+        print("%s has %s health!" % (self.name, self.health))
+
+    def attack(self, villian):
+        villian.health -= self.power
+
+    def block(self, villian):
+        self.health -= villian.power%2
+
+class Villian:
+    def __init__(self, name, health, power):
+        self.name = name
+        self.power = power
+        self.health = health
+
+class Super_Villian(Villian):
+    def __init__(self, name, health, power):
+        self.name = name
+        self.power = power
+        self.health = health
+
+
+#INSTANCES
+#-----------------
+
+
+#INTRO SCREEN
+def intro_screen():
+    print("""
+                               ,,,, 
+                         ,;) .';;;;',
+             ;;,,_,-.-.,;;'_,|I\;;;/),,_
+              `';;/:|:);{ ;;;|| \;/ /;;;\__
+                  L;/-';/ \;;\',/;\/;;;.') ;
+                  .:`''` - \;;'.__/;;;/  . _'-._ 
+                .'/   \     \;;;;;;/.'_7:.  '). \_
+              .''/     | '._ );}{;//.'    '-:  '.,L
+WITH GREAT  .'. /       \  ( |;;;/_/         \._./;\   _,
+POWER,       . /        |\ ( /;;/_/             ';;;\,;;_,
+COMES GREAT             )__(/;;/_/                (;;'''''
+RESPONSIBILITY        _;:':;;;;:';-._             );
+                     /   \  `'`   --.'-._         \/
+                   .'     '.  ,'         '-,
+                  /    /   r--,..__       '.
+                .'    '  .'        '--._     ]
+                (     :.(;>        _ .' '- ;/
+                |      /:;(    ,_.';(   __.'
+                 '- -'"|;:/    (;;;;-'--'
+                       |;/      ;;(
+                       ''      /;;|
+                               \;;|
+                                \/
+""")
+    pause = input("""
+      PRESS ENTER TO PLAY!
+""")
+    print("\033c")
+
+
+#HERO MENU
+def hero_menu():
+    print("""
+--------------------------------------
+| ***** CHOOSE YOUR SUPER HERO ***** |
+--------------------------------------
+|                                    |
+|   Wolverine           Spider-Man   |
+|                                    |
+|   Captain America     Deadpool     |
+|                                    |
+--------------------------------------
+
+WHO DO YOU CHOOSE?
+""")
+
+
+# #VILLIAN SELECTION SCREEN
+def villian_menu():
+    print("""
+--------------------------------------
+| ****** CHOOSE YOUR VILLIAN ******* | 
+--------------------------------------
+|                                    |
+|   Green Goblin       Venom         |
+|                                    |
+|   Loki               Red Skull     |
+|                                    |
+--------------------------------------
+
+WHO WILL YOUR SUPER HERO FIGHT?
+""")
+
+
+#GLOBAL FUNCTIONS
+def main():
+
+    #INTRO
+    intro_screen()
+
+    #HERO SELECTION
+    hero_menu()
+    user_input = input()
+    if user_input == "Wolverine":
+        print()
+        print("You've chosen Wolverine as your super hero!")
+        print()
+        pause = input("Press ENTER to select your VILLIAN!!! >:( ")
+        print()
+        print("\033c")
+    elif user_input == "Captain America":
+        print()
+        print("You've chosen Captain America as your super hero!")
+        print()
+        pause = input("Press ENTER to select your VILLIAN!!! >:( ")
+        print()
+        print("\033c")
+    elif user_input == "Spider-Man":
+        print()
+        print("You've chosen Spider-Man as your super hero!")
+        print()
+        pause = input("Press ENTER to select your VILLIAN!!! >:( ")
+        print()
+        print("\033c")
+    elif user_input == "Deadpool":
+        print()
+        print("You've chosen Deadpool as your super hero! (But is he really though...?)")
+        print()
+        pause = input("Press ENTER to select your VILLIAN!!! >:( ")
+        print()
+        print("\033c")
+    else:
+        print("Who is that???")
+
+    #VILLIAN SELECTION
+    villian_menu()
+    user_input = input()
+    if user_input == "Green Goblin":
+        print()
+        print("You've chosen Green Goblin as your villian...")
+        print()
+    elif user_input == "Loki":
+        print()
+        print("You've chosen Loki as your villian...")
+        print()
+    elif user_input == "Venom":
+        print()
+        print("You've chosen Venom as your villian...")
+        print()
+    elif user_input == "Red Skull":
+        print()
+        print("You've chosen Red Skull as your villian...")
+        print()
+    else:
+        print("Who is that???")
+    
+
+
+
+
+    #OLD CODE
+    # satine.print_status()
+    # monstera.print_status()
+
+    # while monstera.alive() and satine.alive():
+
+    #     print()
+    #     print("Whatchu gonna do mf-er?!")
+    #     print("1. FIGHT!")
+    #     print("2. sleeeep")
+    #     print("3. Ah ShIT! (Run)")
+    #     print(">",)
+    #     user_input = input()
+
+    #     if user_input == "1":
+    #             print()
+    #             print("~Final Fantasy Battle Music Ensues~")
+    #             print()
+    #             satine.attack(monstera)
+    #             monstera.attack(satine)
+       
+    #     elif user_input == "2":
+    #         print()
+    #         print("Sleepy baby... B O N K")
+    #         print()
+    #         satine.sleep(monstera)
+    #         print()
+        
+    #     elif user_input == "3":
+    #         print()
+    #         print("See ya later Weenie Hut Jr!")
+    #         print()
+    #         break
+        
+    #     else:
+    #         print("Wtf is %r???" % user_input)
+
+main()
