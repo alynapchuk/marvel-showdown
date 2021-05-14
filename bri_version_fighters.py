408578ba658847d0a63c1bc599cd8882efc0eff8
Brittany Fisher <bribreana7@gmail.com>
2021-05-14 16:10:08 -0400

    fighters update and abilities

---
+++ bri_version_fighters.py
@@ -0,0 +1,122 @@
+#villians green goblin, loki, venom, red skull
+#heros wolverine, captian america, spiderman, deadpool.
+# I want captain america to throw his shield and spiderman catches a ride on the shield
+
+
+
+from version2_bri import Character
+
+
+boss = {
+
+}
+
+#make edits to above. find a way to make thanos stand out from everyone else.
+
+fighters (Character)= {
+    "name": "Captain America",
+    "alias": "Steve Rogers",
+    "gear": [
+        "Shield",
+        "Super strength"
+    ],
+    "vehicle": {
+        "title": "Valkyrie",
+        "speed": "2000 mph",
+        
+        "name": "Spiderman",
+        "alias": "Peter Parker",
+        "gear": [
+            "mechanical arms",
+            "spider web"
+                ],
+    "vehicle": {
+        "title": "Catch a ride on shield",
+        "speed": "45 mph",
+    }
+    
+    "name": "Wolverine",
+    "alias": "James Howlett aka Logan",
+        }
+    "gear": [
+        "claws",
+        "daggers"
+        ],
+        "vehicle": {
+        "title": "I don %'t need a vehicle!",
+        "speed": "As fast as I want!",
+        #should he fly a helicopter? I don't remember from the movies
+
+        {
+        "name": "deadpool",
+        "alias": "Wade Wilson",
+        "gear": [
+            "sarcasim",
+            "killer humor",
+            "regeneration",
+            "katanas"
+            # Bea and Arthur are his katanas. how do I make him say a punch line?
+        ],
+        "vehicle": {
+            "can someone give me a ride? I'll put Bea and Arthur in the trunk I swear lol"
+        }
+        }
+    }
+     
+
+    }
+}
+
+fighter(Character) = {
+    "name": "Green Goblin",
+    "alias": "Norman Osborn",
+    "gear": [
+        "hoverboard",
+        "pumpkin bombs"
+    ],
+    "vehicle": {
+        "title": "hoverboard",
+        "speed": "90mph",
+    }
+    
+"name": "Loki",
+    "alias": "God of Mischief",
+    "gear": [
+        "Dagger",
+        "Shape Shifter"
+    ],
+    "vehicle": {
+        "title": "I fly peasants!",
+        "speed": "50mph",
+        
+        "name": "Venom",
+        "alias": "Eddie Brock",
+        "gear": [
+            "undetectable by spiderman",
+            "super strength",
+                ],
+    "vehicle": {
+        "title": "swings from buildings",
+        "speed": "30mph",
+
+    "name": "Red Skull",
+    "alias": "Johann Schmidt",
+    }"gear": [
+        "willpower",
+            "reality warp"
+        ],
+    "vehicle": {
+       "title": "teleportation",
+        "speed": "instant",
+# no vehicle really...how to make this better?
+
+hero_name = hero["name"]
+hero_alias = hero.get("alias")
+hero_values = hero.values()
+
+
+
+vilian_name = 
+
+
+# thanos snaps fingers as super power. look up his ship name and his guilt is his kryptonite is killing his daughter. 
---
+++ version2_bri.py
@@ -0,0 +1,90 @@
+#CLASSES
+class Character:
+    def __init__(self, health, power, name):
+        pass
+    def attack(self, target):
+            if isinstance(target, Zombie):
+                print("The attack doesn’t seem to have any effect.")
+            else:
+                target.health -= self.power
+                print("%s does %d damage to %s!" % (self.name, self.power, target.name))
+    def sleep(self, target):
+        self.health -= target.power
+        print("%s does %d damange to %s!" % (target.name, target.power, self.name))
+    def alive(self):
+        if (self.health > 0):
+            return True
+        elif (self.health <= 0):
+            print("RIP in peace, %s..." % self.name)
+            print()
+        elif (target.health) <= 0:
+            print("RIP in peace, %s..." % target.name)
+            print()
+    def print_status(self):
+        print("%s has %d health and %d power." % (self.name, self.health, self.power))
+class Hero(Character):
+    def __init__(self, health, power, name):
+        self.health = health
+        self.power = power
+        self.name = name
+class Goblin(Character):
+    def __init__(self, health, power, name):
+        self.health = health
+        self.power = power
+        self.name = name
+class Zombie(Character):
+    def __init__(self, health, power, name):
+        self.health = health
+        self.power = power
+        self.name = name
+#INSTANCES
+satine = Hero(10, 5, "Satine")
+monstera = Goblin(10, 2, "Monstera")
+zombie = Zombie(666, 100, "Dead Dude")
+def transition():
+    pause = input("PRESS ENTER TO CONTINUE...")
+    print("\033c")
+#GLOBAL FUNCTIONS
+def main():
+    print()
+    print("""
+**************************************
+**************************************
+**                                  **
+***   Welcome to the THUNDERDOME   ***
+**                                  **
+**************************************
+**************************************
+    """)
+    print()
+    transition()
+    satine.print_status()
+    monstera.print_status()
+    while monstera.alive() and satine.alive():
+        print()
+        print("Whatchu gonna do mf-er?!")
+        print("1. FIGHT!")
+        print("2. sleeeep")
+        print("3. Ah ShIT! (Run)")
+        print("">"",)
+        user_input = input()
+        if user_input == "1":
+                print()
+                print("~Final Fantasy Battle Music Ensues~")
+                print()
+                satine.attack(monstera)
+                monstera.attack(satine)
+        elif user_input == "2":
+            print()
+            print("Sleepy baby... B O N K")
+            print()
+            satine.sleep(monstera)
+            print()
+        elif user_input == "3":
+            print()
+            print("See ya later Weenie Hut Jr!")
+            print()
+            break
+        else:
+            print("Wtf is %r???" % user_input)
+main()
